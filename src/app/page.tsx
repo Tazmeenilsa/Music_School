5ed82fb1deeb41eadb38531971a4506bd6e53164
tazmeenilsa <tazmeenilsa@gmail.com>
2025-06-27 18:01:38 +0500

    contact us page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.0]">
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <HeroSection />
       <FeaturedCourses />
       <TestimonialsCard />
